561890e4f42925cf6cf966aeb41b1964635ab28d
adangond <=>
2025-06-02 16:17:41 -0500

    Creacion de tabla de inventario - issue #2

--- MKPro/miApp/admin.py
+++ MKPro/miApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Products, Inventory  # Importa el modelo de productos e inventario
 
 # Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('description', 'category', 'product_class', 'product_type', 'product_UM')
+    search_fields = ('description', 'category')
+    list_filter = ('category',)
+
+admin.site.register(Products, ProductsAdmin)
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'average_cost')
+    search_fields = ('product__description',)
+    list_filter = ('product__category',)
+
+admin.site.register(Inventory, InventoryAdmin)
